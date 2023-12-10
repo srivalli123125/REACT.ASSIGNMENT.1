93996f1ec346b9bd2fb322259989233e640d5860
srivalli123125 <107497023+srivalli123125@users.noreply.github.com>
2023-12-10 22:56:56 +0530

    Add files via upload

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "./utils/FetchFromAPI";
+import { Videos } from ".";
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Result for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
---
+++ src/utils/VideoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import VideoItem from '../components/VideoItem'
+
+const VideoList =(props)=>{
+   const renderedList= props.videos.map((video)=>{
+        return <VideoItem key= {video.id.videoId } onVideoSelect={props.onVideoSelect} vid= {video} />
+    })
+return <div className="ui relaxed divided list">{renderedList}</div>
+}
+
+export default VideoList
